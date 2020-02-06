34265a910c60ce937faa85f9964d7b29decc010b
Jing_Xue <jxue@jxue.corp.beyond.ai>
2020-02-06 14:34:28 -0800

    install flv.js and rtmp server

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import flv from 'flv.js';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
-    return <div>StreamShow</div>
+class StreamShow extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.videoRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+    
+    render() {
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+
+        const {title, description} = this.props.stream;
+        return (
+            <div>
+                <video ref={this.videoRef}/>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        )
+    }
 };
 
-export default StreamShow
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id]
+    };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
