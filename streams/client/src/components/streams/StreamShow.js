94041d02e6184bb305dd9e36910f8756116ca25b
Jing_Xue <jxue@jxue.corp.beyond.ai>
2020-02-06 15:46:09 -0800

    Section 22: Implementing Streaming Video

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -11,18 +11,41 @@ class StreamShow extends React.Component {
     }
 
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id);
+        const { id } = this.props.match.params;
+        this.props.fetchStream(id);
+        this.buildPlayer();
     }
-    
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    componentWillUnmount() {
+        this.player.destroy();
+    }
+
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return;
+        }
+        const { id } = this.props.match.params;
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
+    }
+
     render() {
         if (!this.props.stream) {
             return <div>Loading...</div>
         }
 
-        const {title, description} = this.props.stream;
+        const { title, description } = this.props.stream;
         return (
             <div>
-                <video ref={this.videoRef}/>
+                <video ref={this.videoRef} style={{ width: '100%' }} controls />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
