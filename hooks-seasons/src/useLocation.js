b16c06830f116458d519745a1737031e8b4c145f
Jing_Xue <jxue@jxue.corp.beyond.ai>
2020-02-12 16:22:23 -0800

    Section 26: More Fun with Hooks

---
+++ hooks-seasons/src/useLocation.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+
+export default () => {
+    const [lat, setLat] = useState(null);
+    const [errorMsg, setErrorMsg] = useState("");
+
+    useEffect(() => {
+        window.navigator.geolocation.getCurrentPosition(
+            position => setLat(position.coords.latitude),
+            err => setErrorMsg(err.message)
+        )
+    }, []);
+
+    return [lat, errorMsg];
+}
+
